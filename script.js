1f7358be573e0163c05452124e022b8b9112cc3f
Mohammed Fahad <mfahad24@users.noreply.github.com>
2018-02-01 00:16:04 -0500

    lab 4 partially completed - need sleep :(

--- script.js
+++ script.js
@@ -1,15 +1,34 @@
+// (function () {
+
+startGame();
+
 var i = 0;
 
 function startGame() {
+
   var playGame = prompt("Would you like to play a game with the Almighty Grant?")
   if (playGame === "Yes") {
-    playerName = prompt('Name your character');
+   playerName = prompt('Name your character');
     startCombat();
   }
 }
 
 function startCombat() {
-  var player = {
+
+
+ var attackButton = document.getElementById("attackButton");
+ var healButton = document.getElementById("healButton");
+ var quitButton = document.getElementById("quitButton");
+ var playerHealthBar = document.getElementById("playerHealth");
+ var opponentHealthBar = document.getElementById("opponentHealth");
+ var playerHealsBar = document.getElementById("healCount");
+ var playerWins = document.getElementById("winCount");
+ var messageGame = document.getElementById("message");
+ var startButton1 = document.getElementById("startButton");
+ // var displayDiv1 = document.getElementById("attackdiv");
+ // var displayDiv2 = document.getElementById("statsdiv");
+
+    var player = {
     Name: playerName,
     Health: 40,
     HealsLeft: 2,
@@ -30,42 +49,72 @@ function startCombat() {
       return Math.floor(Math.random() * 4) + 1;
     }
   };
-  while (player.Wins < 5 && player.Health > 0) {
-    attackOrQuit = prompt('Do you want to attack, heal or quit?');
-    if (attackOrQuit === "heal" && player.HealsLeft > 0) {
-      player.Health += player.healsRandom();
-      player.HealsLeft--;
-      console.log(player.Name + " has " + player.HealsLeft + " more healing opportunity left.");
-      console.log(player.Name + " has healed and has " + player.Health + " health.");
+  // startButton1.onclick = function () {}
+  //      startButton1.classList.remove("appear");
+  //      startButton1.classList.add("disappear");
+  //    }
 
-    } else if (attackOrQuit === "attack") {
-      player.Health -= player.attackDamage();
-      opponent.Health -= opponent.attackDamage();
+     attackButton.onclick = function() {
+        player.Health -= player.attackDamage();
+        opponent.Health -= opponent.attackDamage();
+        if (player.Health <= 0) {
+          opponent.Wins++;
+        }
+        if (opponent.Health <= 0) {
+          player.Wins++;
+          opponent.Health = opponent.Health += 10;
+        }
+        updateDisplay();
+        if (player.Wins < 5) {
+          updateMessage(player.Name + " has " + player.Health + " left. Almighty Grant has " + opponent.Health + " left.");
+        } else { updateMessage(player.Name + " has won the game. You have beaten the Almighty Grant. Go get yourself a cookie!")
+      }
+    };
 
-      console.log(player.Name + " has " + player.Health + " left.");
-      console.log("Almighty Grant has " + opponent.Health + " left.");
-    } else if (attackOrQuit === "quit") {
-      break;
+         healButton.onclick = function() {
+      if (player.HealsLeft > 0) {
+        player.Health += player.healsRandom();
+        player.HealsLeft--;
+        updateDisplay();
+        updateMessage(player.Name + " has " + player.HealsLeft + " more healing opportunity left. " + player.Name + " has healed and has " + player.Health + " health.");
+      }
     }
 
-    if (player.Health <= 0) {
-      opponent.Wins++;
-      console.log("Almighty Grant has " + opponent.Wins + " wins.");
-    }
-    if (opponent.Health <= 0) {
-      player.Wins++;
-      opponent.Health = opponent.Health += 10;
-      console.log(opponent.Name + " has regenerated and how has " + opponent.Health + " health points.")
-      console.log(player.Name + " has " + player.Wins + " wins.");
-    }
-    i++;
+  quitButton.onclick = function() {
+    updateMessage(player.Name + " has quit the game. Come play again soon!");
   }
 
-  if (player.Wins === 5) {
-    console.log(player.Name + " has won the game. You have beaten the Almighty Grant. Go get yourself a cookie!");
-  } else if (player.Health <= 0) {
-    console.log("Almighty Grant has won the game. You need to sharpen up your game " + player.Name);
-  } else if (attackOrQuit === "quit") {
-    console.log(player.Name + " has quit the game. Come play again soon!");
-  }
+
+    function updateDisplay() {
+        playerHealthBar.value = player.Health;
+        opponentHealthBar.value = opponent.Health;
+        playerHealsBar.value = player.HealsLeft;
+        playerWins.value = player.Wins;
 }
+
+    function updateMessage(newMessage) {
+        messageGame.innerText = newMessage;
+    }
+  }
+
+//   if (player.Health <= 0) {
+//     opponent.Wins++;
+//     console.log("Almighty Grant has " + opponent.Wins + " wins.");
+//   }
+//   if (opponent.Health <= 0) {
+//     player.Wins++;
+//     opponent.Health = opponent.Health += 10;
+//     console.log(opponent.Name + " has regenerated and now has " + opponent.Health + " health points.")
+//     console.log(player.Name + " has " + player.Wins + " wins.");
+//   }
+// //   i++;
+//   }
+//
+// if (player.Wins === 5) {
+//   console.log(player.Name + " has won the game. You have beaten the Almighty Grant. Go get yourself a cookie!");
+// } else if (player.Health <= 0) {
+//   console.log("Almighty Grant has won the game. You need to sharpen up your game " + player.Name);
+// } else if (attackOrQuit === "quit") {
+//   console.log(player.Name + " has quit the game. Come play again soon!");
+// }
+// })();  // this is for self closing function
